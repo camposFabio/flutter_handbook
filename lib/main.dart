4fc849c18cbd833f81ce4cdbf7366b0d1c78339c
José Fabio Ferraz de Campos <campos_fabio@hotmail.com>
2019-05-15 16:40:32 -0300

    Tab Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,78 @@
 import 'package:flutter/material.dart';
+import './FirstPage.dart' as first;
+import './SecondPage.dart' as second;
+import './ThirdPage.dart' as third;
 
 void main() {
-  runApp(new MaterialApp(
-    home: new MyTextInput()
-  ));
+  runApp(new MaterialApp(home: new MyTabs()));
 }
 
-class MyTextInput extends StatefulWidget {
+class MyTabs extends StatefulWidget {
   @override
-  MyTextInputState createState() => new MyTextInputState();
+  MyTabsState createState() => new MyTabsState();
 }
-  
-  class MyTextInputState extends State<MyTextInput>{
 
-    final TextEditingController controller = new TextEditingController();
+class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
+  TabController controller;
 
-    String result = "";
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(vsync: this, length: 3);
+  }
 
-    @override
-    Widget build(BuildContext context){
-      return new Scaffold(
-        appBar: new AppBar(title: new Text("Input Text"), backgroundColor: Colors.deepOrange),
-        body: new Container(
-          child: new Center(
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new TextField(
-                  decoration: new InputDecoration(
-                    hintText: "Type in here"
-                  ),
-                  //onChanged is called whenever we add or delete something on Text Field
-                  onSubmitted: (String str){
-                    setState((){
-                      result = result + '\n' + str;
-                    });
-                    controller.text = "";
-                  },
-                  controller: controller
-                ),
-                //displaying input text
-                new Text(result)
-                ],
-              ),
-          ),
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Pages"),
+        backgroundColor: Colors.deepOrange,
+        bottom: new TabBar(
+          controller: controller,
+          tabs: <Tab>[
+            new Tab(
+              icon: new Icon(Icons.arrow_forward),
+            ),
+            new Tab(
+              icon: new Icon(Icons.arrow_downward),
+            ),
+            new Tab(
+              icon: new Icon(Icons.arrow_back),
+            )
+          ],
         ),
-      );
-    }
+      ),
+      bottomNavigationBar: new Material(
+        color: Colors.deepOrange,
+        child: new TabBar(
+          controller: controller,
+          tabs: <Tab>[
+            new Tab(
+              icon: new Icon(Icons.arrow_forward),
+            ),
+            new Tab(
+              icon: new Icon(Icons.arrow_downward),
+            ),
+            new Tab(
+              icon: new Icon(Icons.arrow_back),
+            )
+          ],
+        ),
+      ),
+      body: new TabBarView(
+        controller: controller,
+        children: <Widget>[
+          new first.First(),
+          new second.Second(),
+          new third.Third()
+        ],
+      ),
+    );
+  }
 }
