27538ecca1a187474767ef62fbfe2ba82eaf051b
José Fabio Ferraz de Campos <campos_fabio@hotmail.com>
2019-05-15 16:49:50 -0300

    ListView with JSON or List Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
-import 'dart:convert'; //it allows us to convert our json to a list
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(new MaterialApp(home: new HomePage()));
+  runApp(new MaterialApp(
+    home: new HomePage()
+  ));
 }
 
 class HomePage extends StatefulWidget {
@@ -14,42 +16,42 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  //this async func will get data from the internet
-  //when our func is done we return a string
+
+  List data;
+
   Future<String> getData() async {
-    //we have to wait to get the data so we use 'await'
-    http.Response response = await http.get(
-        //Uri.encodeFull removes all the dashes or extra characters present in our Uri
-        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
-        headers: {
-          //if your api require key then pass your key here as well e.g "key": "my-long-key"
-          "Accept": "application/json"
-        });
+    var response = await http.get(
+      Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+      headers: {
+        "Accept": "application/json"
+      }
+    );
 
-    //print(response.body);
-
-    List data = jsonDecode(response.body);
-    //print(data);
-    print(data[1]["title"]); // it will print => title: "qui est esse"
+    this.setState(() {
+      data = jsonDecode(response.body);
+    });
+    
+    print(data[1]["title"]);
+    
+    return "Success!";
   }
 
   @override
-  Widget build(BuildContext context) {
+  void initState(){
+    this.getData();
+  }
+
+  @override
+  Widget build(BuildContext context){
     return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("Stateful Widget!"),
-          backgroundColor: Colors.deepOrange),
-      body: new Center(
-        child: new RaisedButton(
-          child: new Text(
-            "Get data!",
-            style: new TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 20.0),
-          ),
-          onPressed: getData,
-        ),
+      appBar: new AppBar(title: new Text("Listviews"), backgroundColor: Colors.blue),
+      body: new ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index){
+          return new Card(
+            child: new Text(data[index]["title"]),
+          );
+        },
       ),
     );
   }
