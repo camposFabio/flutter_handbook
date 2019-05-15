5114150592d250885ec781ed23948320c3704114
José Fabio Ferraz de Campos <campos_fabio@hotmail.com>
2019-05-15 16:34:20 -0300

    Adding-Deleting text in TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(home: new MyTextInput()));
+  runApp(new MaterialApp(
+    home: new MyTextInput()
+  ));
 }
 
 class MyTextInput extends StatefulWidget {
   @override
   MyTextInputState createState() => new MyTextInputState();
 }
+  
+  class MyTextInputState extends State<MyTextInput>{
 
-class MyTextInputState extends State<MyTextInput> {
-  String result = "";
+    final TextEditingController controller = new TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("Input Text"), backgroundColor: Colors.deepOrange),
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new TextField(
-                decoration: new InputDecoration(hintText: "Type in here"),
-                //onChanged is called whenever we add or delete something on Text Field
-                onSubmitted: (String str) {
-                  setState(
-                    () {
-                      result = str;
-                    },
-                  );
-                },
+    String result = "";
+
+    @override
+    Widget build(BuildContext context){
+      return new Scaffold(
+        appBar: new AppBar(title: new Text("Input Text"), backgroundColor: Colors.deepOrange),
+        body: new Container(
+          child: new Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new TextField(
+                  decoration: new InputDecoration(
+                    hintText: "Type in here"
+                  ),
+                  //onChanged is called whenever we add or delete something on Text Field
+                  onSubmitted: (String str){
+                    setState((){
+                      result = result + '\n' + str;
+                    });
+                    controller.text = "";
+                  },
+                  controller: controller
+                ),
+                //displaying input text
+                new Text(result)
+                ],
               ),
-              //displaying input text
-              new Text(result)
-            ],
           ),
         ),
-      ),
-    );
-  }
+      );
+    }
 }
